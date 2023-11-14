1ebed95d2bf1b7b96e248c0e781b9f7ea15e3553
Lucas Pin <lucasfacundopin@gmail.com>
2023-11-14 20:27:19 +0100

    Cambio en estructura
    Añadi el unsigned int buffer_size a la estructura

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 21:44:53 by lpin              #+#    #+#             */
-/*   Updated: 2023/11/08 19:16:51 by lpin             ###   ########.fr       */
+/*   Updated: 2023/11/14 13:45:42 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ typedef struct s_body
 {
 	int				fd;
 	unsigned int	content_size;
+	unsigned int	buffer_size;
 	int				offset;
 	char			*content;
 	char			*ret;
@@ -37,6 +38,6 @@ unsigned int	ft_strlcat(char *dst, const char *src, unsigned int dstsize);
 
 void			ft_destroyer(char *to_destroy);
 
-char			*ft_put_forward(char *src, int len);
+char			*ft_put_forward(char *src, char *dst, int len);
 
 #endif
