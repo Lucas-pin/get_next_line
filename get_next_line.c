d40f611f8dafb433b7b14d35e130bcc6d77ec456
Lucas Pin <lucasfacundopin@gmail.com>
2023-11-14 20:29:37 +0100

    Modifique ft_put_forward y cree ft_read
    Cambie la lógica de ft_put_Forward realizando que moviera la primera
    linea a un puntero *dst y reacomodara el puntero *src.
    Inicie la lógica de ft_read según el diagrama de flujo.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 22:05:06 by lpin              #+#    #+#             */
-/*   Updated: 2023/11/08 19:16:20 by lpin             ###   ########.fr       */
+/*   Updated: 2023/11/14 14:11:22 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,84 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-char	*ft_put_forward(char *src, int len)
+char	*ft_put_forward(char *src, char *dst, int len)
 {
-	int	i;
+	int		i;
+	int		j;
+	char	*temp;
 
-	i = len;
+	i = ft_strlen(src);
+	j = 0;
+	temp = src;
 	if (!src || len == 0 || len == -1)
 		return (src);
-	while (i > 0)
+	while (i >= len)
 	{
+		if (j < len)
+			dst[j] = *src;
 		*src = *(src + len);
 		--i;
+		++j;
 		src++;
 	}
-	return (src);
+	dst[j] = '\0';
+	while (*src)
+	{
+		*src = '\0';
+		src++;
+	}
+	return (temp);
+}
+
+void	ft_read(t_body *body, char *buffer)
+{
+	if (body->content == NULL)
+	{
+		body->content = malloc((sizeof(char) * (body->buffer_size + 1)));
+		if (body->content == NULL)
+		{
+			ft_destroyer(body->content);
+			return ;
+		}
+	}
+	ft_strlcat(body->content, buffer, (body->buffer_size + 1));
 }
 
 char	*get_next_line(int fd)
 {
-	static t_body	body = {0, 0, 0, NULL, NULL};
-	char			buffer[1000];
+	static t_body	body = {0, 0, 0, 0, NULL, NULL};
+	char			buffer[17];
+	int				i;
 
-	read(fd, buffer, sizeof(buffer));
-	body.offset = ft_searcher(buffer);
-	body.content = buffer;
+	while (ft_searcher(body.content) <= 0)
+	{
+		i = sizeof(buffer);
+		while (i >= 0)
+		{
+			buffer[i] = '\0';
+			--i;
+		}
+		if (read(fd, buffer, sizeof(buffer)) == -1)
+			return (NULL);
+		if (ft_strlen(buffer) > sizeof(buffer))
+			body.buffer_size = sizeof(buffer);
+		else
+			body.buffer_size = ft_strlen(buffer);
+		ft_read(&body, buffer);
+	}
+	printf("ft_searcher(body.content): %i\n", ft_searcher(body.content));
+	body.ret = malloc (ft_searcher(body.content) + 1);
+	ft_put_forward(body.content, body.ret, ft_searcher(body.content));
 	return (body.ret);
 }
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
+	char	*ret;
 
 	fd = open("prueba.txt", O_RDONLY);
-	get_next_line(fd);
+	ret = get_next_line(fd);
+	printf("El ret es: %s", ret);
 	return (0);
 }
