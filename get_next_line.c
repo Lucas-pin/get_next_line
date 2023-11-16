c127941308b1ef603d7b05c0ca58aa0b9ab91139
Lucas-pin <Lucasfacundopin@gmail.com>
2023-11-16 11:55:35 +0100

    Modifique ft_put_forward ya que reutilice ft_strlcat

--- get_next_line.c
+++ get_next_line.c
@@ -3,38 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpin <lpin@student.42.malaga.com>          +#+  +:+       +#+        */
+/*   By: lpin < lpin@student.42malaga.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 22:05:06 by lpin              #+#    #+#             */
-/*   Updated: 2023/11/14 14:11:22 by lpin             ###   ########.fr       */
+/*   Updated: 2023/11/16 11:54:16 by lpin             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
 
-char	*ft_put_forward(char *src, char *dst, int len)
+char	*ft_put_forward(char *src, int len)
 {
 	int		i;
-	int		j;
 	char	*temp;
 
 	i = ft_strlen(src);
-	j = 0;
 	temp = src;
 	if (!src || len == 0 || len == -1)
 		return (src);
 	while (i >= len)
 	{
-		if (j < len)
-			dst[j] = *src;
 		*src = *(src + len);
 		--i;
-		++j;
 		src++;
 	}
-	dst[j] = '\0';
 	while (*src)
 	{
 		*src = '\0';
@@ -60,7 +52,7 @@ void	ft_read(t_body *body, char *buffer)
 char	*get_next_line(int fd)
 {
 	static t_body	body = {0, 0, 0, 0, NULL, NULL};
-	char			buffer[17];
+	char			buffer[18];
 	int				i;
 
 	while (ft_searcher(body.content) <= 0)
@@ -79,9 +71,10 @@ char	*get_next_line(int fd)
 			body.buffer_size = ft_strlen(buffer);
 		ft_read(&body, buffer);
 	}
-	printf("ft_searcher(body.content): %i\n", ft_searcher(body.content));
 	body.ret = malloc (ft_searcher(body.content) + 1);
-	ft_put_forward(body.content, body.ret, ft_searcher(body.content));
+	ft_strlcat(body.ret, body.content, ft_searcher(body.content) + 1);
+	ft_put_forward(body.content, ft_searcher(body.content));
+	printf("El body.content es: %s", body.content);
 	return (body.ret);
 }
 
